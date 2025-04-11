72480f047889d6b1fd84d19fb306ff129d771056
haupjun <dqws304@gmail.com>
2025-04-11 16:40:08 +0900

    add even numbers

--- mynumber.c
+++ mynumber.c
@@ -5,6 +5,10 @@ int main() {
         if (i % 2 == 1)
             printf("%d\n", i);
     }
+    for (int i = 1; i <= 100; i++) {
+        if (i % 2 == 0)
+            printf("%d\n", i);
+    }
     return 0;
 }
 
